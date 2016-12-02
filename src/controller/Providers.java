807a3db2e226c2139761571fbf68bed2b34d56a4
abhinav.v <abhinav.v@endurance.com>
2016-12-03 04:28:39 +0530

    Signing up provider using DB

--- src/controller/Providers.java
+++ src/controller/Providers.java
@@ -1,6 +1,7 @@
 package controller;
 
 import model.Token;
+import util.Database;
 import util.JsonString;
 
 import javax.servlet.ServletException;
@@ -9,6 +10,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Logger;
 
 /**
@@ -33,7 +38,42 @@ public class Providers extends HttpServlet
         }
         else
         {
-            out.print(JsonString.of(new Token(username + "~" + password)));
+            try (PreparedStatement pst = Database.getConnection().prepareStatement(
+                    "INSERT INTO provider (username, password, token) VALUES (?, ?, UUID())",
+                    Statement.RETURN_GENERATED_KEYS))
+            {
+                pst.setString(1, username);
+                pst.setString(2, password);
+
+                pst.executeUpdate();
+                final ResultSet generatedKeys = pst.getGeneratedKeys();
+
+                int providerId = -1;
+                if (generatedKeys.next())
+                {
+                    providerId = generatedKeys.getInt(1);
+                }
+
+                final Statement statement = Database.getConnection().createStatement();
+                final boolean execute = statement.execute(
+                        "SELECT token FROM provider WHERE id = " + providerId);
+
+                String token = null;
+                if (execute)
+                {
+                    final ResultSet resultSet = statement.getResultSet();
+                    if (resultSet.next())
+                    {
+                        token = resultSet.getString("token");
+                    }
+                }
+
+                out.print(JsonString.of(new Token(token)));
+            }
+            catch (SQLException e)
+            {
+                e.printStackTrace();
+            }
         }
     }
 
